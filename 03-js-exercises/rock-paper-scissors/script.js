d5db7e7dabc210530efbddc00998aaa18f4b45ef
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-04 00:36:31 +0800

    ADD: added 5-round function

--- 03-js-exercises/rock-paper-scissors/script.js
+++ 03-js-exercises/rock-paper-scissors/script.js
@@ -12,14 +12,22 @@ function getPlayerChoice() {
     return (choice[0].toUpperCase() + choice.slice(1).toLowerCase());
 }
 
-function playRound() {
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
-
+function playRound(playerSelection, computerSelection) {
     let result = (playerSelection === computerSelection) ? 2 :
     ((playerSelection === "Rock" && computerSelection === "Scissors") || 
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")) ? 1 : 0;
 
     return result;
+}
+
+function game() {
+    for (let i=0; i<5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        (result === 0) ? console.log(`You Lose! ${computerSelection} beats ${playerSelection}`) :
+        (result === 1) ? console.log(`You Win! ${playerSelection} beats ${computerSelection}`) :
+        (result === 2) ? console.log(`You Tied! You both chose ${playerSelection}`) : 0;
+    }
 }

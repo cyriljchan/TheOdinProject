279e7728d011dcfd106313dca2712dc41c7a35c7
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-04 00:07:46 +0800

    ADD: added win-lose-tie function

--- 03-js-exercises/rock-paper-scissors/script.js
+++ 03-js-exercises/rock-paper-scissors/script.js
@@ -1,13 +1,24 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
     }
 }
 
 function getPlayerChoice() {
     let choice = prompt("Please type rock, paper, or scissors");
-    return choice.toLowerCase();
+    return (choice[0].toUpperCase() + choice.slice(1).toLowerCase());
+}
+
+function playRound(playerSelection, computerSelection) {
+    let tie = (playerSelection === computerSelection);
+    let win = ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper"));
+
+    tie ? console.log(`You Tied! You both chose ${playerSelection}`) :
+    win ? console.log(`You Win! ${playerSelection} beats ${computerSelection}`) : 
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
 }

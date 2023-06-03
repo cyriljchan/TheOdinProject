0131649786318c40032fd2ca5ff6e6aab139549b
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-03 23:28:31 +0800

    UPDATE: lowercase computer choice

--- 03-js-exercises/rock-paper-scissors/script.js
+++ 03-js-exercises/rock-paper-scissors/script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
     }
 }

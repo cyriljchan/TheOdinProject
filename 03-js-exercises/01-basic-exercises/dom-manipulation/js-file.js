f54766c9c5dc3952ba5fe82ea151bda9b6ec93d9
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-08 22:14:52 +0800

    UPDATE: solution to dom-manipulation

--- 03-js-exercises/01-basic-exercises/dom-manipulation/js-file.js
+++ 03-js-exercises/01-basic-exercises/dom-manipulation/js-file.js
@@ -5,3 +5,27 @@
 //   - another <h1> that says “I’m in a div”
 //   - a <p> that says “ME TOO!”
 //   - Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+
+const container = document.querySelector('#container');
+
+const par = document.createElement('p');
+par.textContent = "Hey I\'m red!";
+par.setAttribute('style', 'color: red;');
+container.appendChild(par);
+
+const h3 = document.createElement('h3');
+h3.textContent = "I\'m a blue h3!";
+h3.setAttribute('style', 'color: blue;');
+container.appendChild(h3);
+
+const customDiv = document.createElement('div');
+customDiv.setAttribute('style', 'background-color:pink; border: solid black;');
+
+const h1 = document.createElement('h1');
+h1.textContent = "I\'m in a div";
+customDiv.appendChild(h1);
+
+par.textContent = "ME TOO!";
+customDiv.appendChild(par);
+
+container.appendChild(customDiv);

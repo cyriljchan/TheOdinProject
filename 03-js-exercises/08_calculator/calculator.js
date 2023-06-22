9469f30ef52e99432f6c4cdc3ff6240f4c3a8373
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-22 23:43:06 +0800

    solution to 08_calculator

--- 03-js-exercises/08_calculator/calculator.js
+++ 03-js-exercises/08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(num => sum += num);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach(num => prod *= num);
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let prod = 1;
+  for (let i=0; i<y; i++) {prod *= x};
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let prod = 1;
+	if (x === 0) {return prod};
+  for (let i=x; i>0; i--){prod *= i};
+  return prod;
 };
 
 // Do not edit below this line

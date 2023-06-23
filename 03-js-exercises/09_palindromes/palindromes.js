e46ce737d1479ebf096f79655572d199e529235b
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-23 21:53:27 +0800

    solution to 09_palindromes

--- 03-js-exercises/09_palindromes/palindromes.js
+++ 03-js-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let len = string.length;
+    for (let i=0; i<Math.floor(len/2); i++){
+        console.log(string[i], string[len-1-i]);
+        if (string[i] !== string[len-1-i]) {return false}
+    }
+    return true;
 };
 
 // Do not edit below this line

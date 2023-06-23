ca87e9853405757b2d011c56fd64ccf1809b4b43
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-24 00:00:22 +0800

    solution to 10_fibonacci

--- 03-js-exercises/10_fibonacci/fibonacci.js
+++ 03-js-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {return "OOPS"};
+    if (n === 0) {return 0};
 
+    let fib = [1, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib.pop();
 };
 
 // Do not edit below this line

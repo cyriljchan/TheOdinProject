23b8397084743da9bfef7c3a127badbb551f429d
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-24 16:17:40 +0800

    solution to 12_findTheOldest

--- 03-js-exercises/12_findTheOldest/findTheOldest.js
+++ 03-js-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const getAge = function(person) {
+    return (!person['yearOfDeath']) ? new Date().getFullYear() - person['yearOfBirth'] : person['yearOfDeath'] - person['yearOfBirth'];
+  };
+  
+  const findTheOldest = function(people) {
+    return people.reduce((prev, curr) => {
+      prev['age'] = getAge(prev);
+      curr['age'] = getAge(curr);
+      return (Math.max(prev['age'], curr['age']) === prev['age']) ? prev : curr;
+    });
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
